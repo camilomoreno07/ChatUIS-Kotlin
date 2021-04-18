1711a9ae5ab304f124ee083eab339b1fed29eb3b
Jean <j.carlo0123@gmail.com>
2021-04-17 19:01:01 -0500

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/example/chatuis/MainActivity.kt
+++ app/src/main/java/com/example/chatuis/MainActivity.kt
@@ -14,13 +14,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+<<<<<<< Updated upstream
         textUsuario = findViewById(R.id.usuario)
         textContraseña = findViewById(R.id.contraseña)
         btn_ingresar = findViewById(R.id.boton)
         btn_ingresar.setOnClickListener{
             Toast.makeText(this, textContraseña.text, Toast.LENGTH_SHORT).show()
         }
-    }
+=======
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+    }
 
 }
